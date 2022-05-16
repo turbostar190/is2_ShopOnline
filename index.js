82228c000f3a1101f9f4d1856ea952fb999de838
FraGirla <66215169+FraGirla@users.noreply.github.com>
2022-05-16 17:05:26 +0200

    added connection to database

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
-const express = require('express')
-const app = express()
-const port = 3000
+require('dotenv').config()
+const mongoose = require('mongoose');
+const express = require('express');
 
+const port = process.env.PORT || 3000;
+
+const app = express()
+
+// TODO: refactor routing
 app.get('/api/', (req, res) => {
   res.send('Hello World!')
 })
@@ -11,7 +16,17 @@ app.use('/', express.static('public'));
 app.use(express.json()); //Used to parse JSON bodies
 app.use(express.urlencoded({ extended: true })); //Parse URL-encoded bodies
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
-
+/**
+ * Configure mongoose
+ */
+// mongoose.Promise = global.Promise;
+app.locals.db = mongoose.connect(process.env.DB_URL, {useNewUrlParser: true, useUnifiedTopology: true})
+.then ( () => {
+    
+    console.log("Connected to Database");
+    
+    app.listen(port, () => {
+        console.log(`Server listening on port ${port}`);
+    });
+    
+});
