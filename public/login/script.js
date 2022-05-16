c98c221672754c83c7bd9b9548f9b4fc6d311f42
FraGirla <66215169+FraGirla@users.noreply.github.com>
2022-05-17 00:30:32 +0200

    Starting implementing frontend login function

--- public/login/script.js
+++ public/login/script.js
@@ -1,6 +1,28 @@
-let form = document.querySelecter('form');
+const loginForm = document.getElementById("login-form");
+const loginButton = document.getElementById("login-form-submit");
+const loginErrorMsg = document.getElementById("login-error-msg");
 
-form.addEventListener('submit', (e) => {
-  e.preventDefault();
-  return false;
-});
+//login function with api call
+function login() {
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+  const data = {
+    email: email,
+    password: password
+  };
+  fetch("/login", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(data)
+  })
+    .then(res => res.json())
+    .then(res => {
+      if (res.success) {
+        window.location.href = "/";
+      } else {
+        loginErrorMsg.innerHTML = res.msg;
+      }
+    });
+}
