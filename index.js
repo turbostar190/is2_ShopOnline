e2c3e9a2d154c50fe6852327d5a1b6ad661760b0
FraGirla <66215169+FraGirla@users.noreply.github.com>
2022-05-17 16:25:29 +0200

    frontend and index.js cleanup

--- index.js
+++ index.js
@@ -32,17 +32,33 @@ app.use(express.json()); //Used to parse JSON bodies
 app.use(express.urlencoded({ extended: true })); //Parse URL-encoded bodies
 const cors = require('cors')
 app.use(cors())
+
 /**
  * Configure mongoose
  */
 // mongoose.Promise = global.Promise;
-app.locals.db = mongoose.connect(process.env.DB_URL, {useNewUrlParser: true, useUnifiedTopology: true})
-.then ( () => {
-    
+app.locals.db = mongoose.connect(process.env.DB_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+
     console.log("Connected to Database");
-    
+
     app.listen(port, () => {
-        console.log(`Server listening on port ${port}`);
+      console.log(`Server listening on port ${port}`);
     });
-    
-});
+
+    app.use('/users', usersRouter);
+    // catch 404 and forward to error handler
+    app.use(function (req, res, next) {
+      res.status(err.status || 404).json({
+        message: "No such route exists"
+      })
+    });
+
+    // error handler
+    app.use(function (err, req, res, next) {
+      res.status(err.status || 500).json({
+        message: "Error Message"
+      })
+    });
+
+  });
