214654690ee6a1e3b57546b195a39777bf9c8421
FraGirla <66215169+FraGirla@users.noreply.github.com>
2022-05-18 17:14:19 +0200

    Init frontend

---
+++ public/edit-product/script.js
@@ -0,0 +1,33 @@
+window.onload = function () {
+    const loginForm = document.getElementById("login-formaggio");
+    loginForm.addEventListener("submit", edit_product);
+  }
+  
+  //login function with api call
+  function edit_product(e) {
+    e.preventDefault();
+    console.log("init edit")
+    const name = document.getElementById("name").value;
+    const description = document.getElementById("description").value;
+    const cost = document.getElementById("cost").value;
+    const img = document.getElementById("img").value;
+
+    const data = {
+      name: name,
+      description: description,
+      cost : cost,
+    };
+    console.log(data);
+    $.ajax({
+      url: "http://localhost:3000/products/",
+      type: "put",
+      dataType: "json",
+      data: data,
+      success: function (result) {
+        console.log(result);
+      },
+      error: function (request, status, error) {
+        alert(request.responseText);
+      }
+    });
+  }
