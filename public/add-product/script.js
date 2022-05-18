48f0af4109ab25d7f004e68d2adf6050bbc8f21e
FraGirla <66215169+FraGirla@users.noreply.github.com>
2022-05-18 18:42:00 +0200

    Fix upload files frontend Co-authored-by: Federico C. <turbostar190@users.noreply.github.com>

--- public/add-product/script.js
+++ public/add-product/script.js
@@ -6,23 +6,18 @@ window.onload = function () {
   //login function with api call
   function add_product(e) {
     e.preventDefault();
-    console.log("init add")
-    const name = document.getElementById("name").value;
-    const description = document.getElementById("description").value;
-    const cost = document.getElementById("cost").value;
-    const img = document.getElementById("img").value;
-
-    const data = {
-      name: name,
-      description: description,
-      cost : cost,
-    };
-    console.log(data);
+    console.log("init add");
+    var form = $('#login-formaggio')[0]; 
+    var formData = new FormData(form);
+    console.log(formData);
     $.ajax({
-      url: "http://localhost:3000/products/",
+      url: "http://127.0.0.1:3000/products/",
       type: "post",
-      dataType: "json",
-      data: data,
+      enctype: 'multipart/form-data',
+      data: formData,
+      contentType: false,
+      processData: false,
+      cache : false,
       success: function (result) {
         console.log(result);
       },
