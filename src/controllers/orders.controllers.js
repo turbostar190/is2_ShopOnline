e46e2c624c19675e333bf62d7d426502fc02aa47
FraGirla <66215169+FraGirla@users.noreply.github.com>
2022-05-21 20:56:31 +0200

    Implemented approve and notApprove methods

--- src/controllers/orders.controllers.js
+++ src/controllers/orders.controllers.js
@@ -37,7 +37,7 @@ function postOrders(req, res, next) {
                     userId: req.user.userId,
                     userName : req.user.nome,
                     timestamp: _id.getTimestamp(),
-                    accepted : false,
+                    accepted : null,
                 }
                 console.log(req.user.nome);
                 const order = new Order(order_data);
@@ -137,8 +137,42 @@ function approveOrder(req, res, next){
     });
 }
 
+function notApproveOrder(req, res, next){
+    console.log("init approve");
+    Order.findById(req.params.id)
+    .exec()
+    .then((order) => {
+        if(order != null){
+            order.accepted = false;
+            order
+            .save()
+            .then((result) => {
+                res.status(200).json(result);
+            })
+            .catch((err) => {
+                console.log(err)
+                res.status(500).json({
+                    message: err.toString()
+                })
+            });
+        }else{
+            return res.status(404).json({
+                message: "Order not found",
+            });
+        }
+    })
+    .catch((err) => {
+        console.log(err)
+        res.status(500).json({
+            message: err.toString()
+        })
+    });
+}
+
+
 module.exports = {
     postOrders,
     getOrders,
-    approveOrder
+    approveOrder,
+    notApproveOrder
 }
--- src/routes/orders.routes.js
+++ src/routes/orders.routes.js
@@ -4,6 +4,7 @@ const checkAuth = require('../middleware/checkAuth.middleware')
 const router = express.Router();
 
 router.post('/', checkAuth, ordersControllers.postOrders);
-router.put('/:id', checkAuth, ordersControllers.approveOrder);
+router.put('/approve/:id', checkAuth, ordersControllers.approveOrder);
+router.put('/not_approve/:id', checkAuth, ordersControllers.notApproveOrder);
 router.get('/', checkAuth, ordersControllers.getOrders);
 module.exports = router;
