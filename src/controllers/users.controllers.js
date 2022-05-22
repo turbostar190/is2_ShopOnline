1ab96bf4d7ca73fd1fabc52f89491f4fdde5101b
FedeC <24813315+turbostar190@users.noreply.github.com>
2022-05-22 13:35:08 +0200

    User backend fix return value

--- src/controllers/users.controllers.js
+++ src/controllers/users.controllers.js
@@ -61,10 +61,7 @@ const getMe = async (req, res) => {
     const userId = req.user.userId;
     const user = await User.findById(userId);
     if (user) {
-        res.status(200).json({
-            message: "Found",
-            user,
-        });
+        res.status(200).json(user);
     } else {
         res.status(400).json({
             message: "Bad request",
@@ -160,5 +157,5 @@ module.exports = {
     userSignIn,
     userLogin,
     getMe,
-    checkToken
+    checkToken,
 };
