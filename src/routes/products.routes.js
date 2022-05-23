32734330c23465ce6fa00e837851e0cbe0cf6480
FraGirla <66215169+FraGirla@users.noreply.github.com>
2022-05-23 15:38:20 +0200

    Added products api documentation

--- src/routes/products.routes.js
+++ src/routes/products.routes.js
@@ -15,8 +15,113 @@ var storage = multer.diskStorage({
 
 var upload = multer({ storage: storage });
 
+/**
+* @openapi
+* /v1/products/:
+*   post:
+*     description: Aggiunge un prodotto
+*     produces:
+*       - application/json
+*     security:
+*       - token: []
+*     requestBody:
+*       required: true
+*       content:
+*         application/json:
+*           schema:
+*             type: object
+*             properties:
+*               name:
+*                 type: string
+*               description:
+*                 type: string
+*               category:
+*                 type: string
+*               cost:
+*                 type: number
+*               img:
+*                 type: string
+*     responses:
+*       201:
+*         description: Ritorna il percorso della risorsa creata.
+*       400:
+*         description: Parametri mancanti.
+*       401:
+*         description: Non autorizzato.
+*       500:
+*         description: Errore interno.
+*/
 router.post('/', checkAuth, upload.single('img'), productControllers.postProducts);
+
+/**
+* @openapi
+* /v1/products/:
+*   get:
+*     description: Ottiene la lista dei prodotti
+*     produces:
+*       - application/json
+*     responses:
+*       200:
+*         description: Ottiene la lista dei prodotti.
+*       500:
+*         description: Errore interno.
+*/
 router.get('/', productControllers.getProducts);
+
+/**
+* @openapi
+* /v1/products/:id:
+*   get:
+*     description: Ottiene prodotto per id
+*     produces:
+*       - application/json
+*     responses:
+*       200:
+*         description: Ottiene il prodotto che corrisponde all'id passato.
+*       404:
+*         description: Prodotto non trovato.
+*       500:
+*         description: Errore interno.
+*/
 router.get('/:id', productControllers.getProductById);
+
+/**
+* @openapi
+* /v1/products/:id:
+*   put:
+*     description: Modifica il prodotto corrispondente all'id passato.
+*     produces:
+*       - application/json
+*     security:
+*       - token: []
+*     requestBody:
+*       required: true
+*       content:
+*         application/json:
+*           schema:
+*             type: object
+*             properties:
+*               name:
+*                 type: string
+*               description:
+*                 type: string
+*               category:
+*                 type: string
+*               cost:
+*                 type: number
+*               img:
+*                 type: string
+*     responses:
+*       200:
+*         description: Ottiene il prodotto che corrisponde all'id passato.
+*       400:
+*         description: Parametri mancanti.
+*       401:
+*         description: Non autorizzato.
+*       404:
+*         description: Prodotto non trovato.
+*       500:
+*         description: Errore interno.
+*/
 router.put('/:id', checkAuth, upload.single('img'), productControllers.editProducts);
 module.exports = router
