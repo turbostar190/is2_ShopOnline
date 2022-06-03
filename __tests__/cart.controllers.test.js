88ede2851f8d1f29b73fd022b0d52165fd418c8e
Marco Giacopuzzi <29371122+mgiacopu@users.noreply.github.com>
2022-06-03 11:14:03 +0200

    refactor to use mock

--- __tests__/cart.controllers.test.js
+++ __tests__/cart.controllers.test.js
@@ -11,30 +11,9 @@ const jwt = require('jsonwebtoken'); // used to create, sign, and verify tokens
 const { app, server } = require('../index');
 
 const { connectDB, disconnectDB } = require('../database');
-const mongoose = require('mongoose');
-const bcrypt = require("bcrypt");
-const Product = require('../src/models/products');
-const User = require('../src/models/users');
+const { NORMAL_USER, ADMIN_USER, TEST_PRODUCT, TEST_PASSWORD } = require('../src/mock');
 
-var TOKEN;
-
-const test_product = new Product({
-    _id: new mongoose.Types.ObjectId(),
-    name: 'Test Product',
-    description: 'Test Description',
-    cost: 10,
-    category: 'Test Category',
-    nome: 'Test User',
-});
-
-const user = new User({
-    _id: new mongoose.Types.ObjectId(),
-    email: "test@test.test",
-    password: bcrypt.hashSync("new-password", 10),
-    nome: "Test User",
-    indirizzo: null,
-});
-
+var NORMAL_TOKEN;
 
 test('app module should be defined', () => {
     expect(app).toBeDefined();
@@ -45,18 +24,18 @@ beforeAll(async () => {
     jest.setTimeout(10000);
     connectDB();
 
-    test_product.save(); // save test product to database
-    user.save(); // save test user to database
+    TEST_PRODUCT.save(); // save test product to database
+    NORMAL_USER.save(); // save test user to database
 
     // retrieve token for authentication
     const login = await request(app)
         .post('/api/v1/users/login')
         .send({
-            email: user.email,
-            password: "new-password",
+            email: NORMAL_USER.email,
+            password: TEST_PASSWORD,
         })
 
-    TOKEN = login.body.token;
+    NORMAL_TOKEN = login.body.token;
 
 });
 
@@ -68,66 +47,57 @@ afterAll(async () => {
 describe('POST /api/v1/cart', () => {
 
     it('Add OK', async () => {
-
         const response = await request(app)
             .post('/api/v1/cart')
-            .set('Authorization', `Bearer ${TOKEN}`)
+            .set('Authorization', `Bearer ${NORMAL_TOKEN}`)
             .send({
-                productId: test_product._id,
+                productId: TEST_PRODUCT._id,
                 quantity: 3
             });
-
         expect(response.statusCode).toBe(201);
-
     });
 
     it('Modify quantity OK', async () => {
         const response = await request(app)
             .post('/api/v1/cart')
-            .set('Authorization', `Bearer ${TOKEN}`)
+            .set('Authorization', `Bearer ${NORMAL_TOKEN}`)
             .send({
-                productId: test_product._id,
+                productId: TEST_PRODUCT._id,
                 quantity: 10
             });
-
         expect(response.statusCode).toBe(200);
-
     });
 
     it('Add unknown ID', async () => {
         const response = await request(app)
             .post('/api/v1/cart')
-            .set('Authorization', `Bearer ${TOKEN}`)
+            .set('Authorization', `Bearer ${NORMAL_TOKEN}`)
             .send({
                 productId: 'not-working-id',
                 quantity: 10
             });
-
         expect(response.statusCode).toBe(404);
-
     });
+
     it('Missing Parameters', async () => {
         const response = await request(app)
             .post('/api/v1/cart')
-            .set('Authorization', `Bearer ${TOKEN}`)
+            .set('Authorization', `Bearer ${NORMAL_TOKEN}`)
             .send({
                 quantity: 10
             });
-
         expect(response.statusCode).toBe(400);
-
     });
+
     it('Modify with negative quantity', async () => {
         const response = await request(app)
             .post('/api/v1/cart')
-            .set('Authorization', `Bearer ${TOKEN}`)
+            .set('Authorization', `Bearer ${NORMAL_TOKEN}`)
             .send({
                 productId: 'zzzzzzzzzzzzzzz',
                 quantity: -1
             });
-
         expect(response.statusCode).toBe(400);
-
     });
 })
 
@@ -142,7 +112,7 @@ describe('GET /api/v1/cart', () => {
     it('Logged OK', async () => {
         const response = await request(app)
             .get('/api/v1/cart')
-            .set('Authorization', `Bearer ${TOKEN}`)
+            .set('Authorization', `Bearer ${NORMAL_TOKEN}`)
         expect(response.status).toBe(200);
     });
 });
@@ -151,7 +121,7 @@ describe('GET /api/v1/cart/quantity', () => {
     it('OK', async () => {
         const response = await request(app)
             .get('/api/v1/cart/quantity')
-            .set('Authorization', `Bearer ${TOKEN}`);
+            .set('Authorization', `Bearer ${NORMAL_TOKEN}`);
         expect(response.status).toBe(200);
     });
 });
@@ -162,9 +132,9 @@ describe('PATCH /api/v1/cart/', () => {
     it('OK', async () => {
         const response = await request(app)
             .patch(`/api/v1/cart/`)
-            .set('Authorization', `Bearer ${TOKEN}`)
+            .set('Authorization', `Bearer ${NORMAL_TOKEN}`)
             .send({
-                productId: test_product._id,
+                productId: TEST_PRODUCT._id,
                 quantity: 1
             });
         expect(response.status).toBe(200);
@@ -173,7 +143,7 @@ describe('PATCH /api/v1/cart/', () => {
     it('Modify unknown ID', async () => {
         const response = await request(app)
             .patch('/api/v1/cart/')
-            .set('Authorization', `Bearer ${TOKEN}`)
+            .set('Authorization', `Bearer ${NORMAL_TOKEN}`)
             .send({
                 quantity: 1,
                 productId: 'not-working-id',
@@ -184,20 +154,19 @@ describe('PATCH /api/v1/cart/', () => {
 
 describe('DELETE /api/v1/cart/', () => {
 
-    // delete product without id
     it('No ID provided', async () => {
         const response = await request(app)
             .delete('/api/v1/cart/')
-            .set('Authorization', `Bearer ${TOKEN}`);
+            .set('Authorization', `Bearer ${NORMAL_TOKEN}`);
         expect(response.status).toBe(400);
     });
 
     it('OK', async () => {
         const response = await request(app)
             .delete(`/api/v1/cart/`)
-            .set('Authorization', `Bearer ${TOKEN}`)
+            .set('Authorization', `Bearer ${NORMAL_TOKEN}`)
             .send({
-                productId: test_product._id,
+                productId: TEST_PRODUCT._id,
             });
         expect(response.status).toBe(200);
     });
@@ -205,7 +174,7 @@ describe('DELETE /api/v1/cart/', () => {
     it('Unknown ID', async () => {
         const response = await request(app)
             .delete('/api/v1/cart/')
-            .set('Authorization', `Bearer ${TOKEN}`)
+            .set('Authorization', `Bearer ${NORMAL_TOKEN}`)
             .send({
                 productId: 'wrong-product-id',
             });
