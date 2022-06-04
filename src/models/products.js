b5e98560c215fc8f4b88ff71cbe7888e8a64ce6c
FraGirla <66215169+FraGirla@users.noreply.github.com>
2022-06-04 09:18:07 +0200

    Updated products model

--- src/models/products.js
+++ src/models/products.js
@@ -1,9 +1,15 @@
 const mongoose = require('mongoose');
 
-const productSchema = new mongoose.Schema({ name: String, description : String, cost : Number, category : String, nome : String, img:
-    {
+const productSchema = new mongoose.Schema({
+    name: { type : String , required : true},
+    description: { type : String , required : true, default: "No description provided"},
+    cost: {type : Number, min : 0, required : true},
+    category: {type : String, required : true},
+    img: {
         data: String,
-        contentType: String
-    }})
+        contentType: String,
+        required: true
+    }
+})
 
 module.exports = mongoose.model("Product", productSchema);
