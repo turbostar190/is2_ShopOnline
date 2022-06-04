7876c6a3fafb0b98b0af84b10e558c4f708a8ce8
FraGirla <66215169+FraGirla@users.noreply.github.com>
2022-06-04 09:26:54 +0200

    Fix products model

--- src/models/products.js
+++ src/models/products.js
@@ -1,15 +1,16 @@
 const mongoose = require('mongoose');
 
 const productSchema = new mongoose.Schema({
-    name: { type : String , required : true},
-    description: { type : String , required : true, default: "No description provided"},
-    cost: {type : Number, min : 0, required : true},
-    category: {type : String, required : true},
+    name: { type: String, required: true },
+    description: { type: String, required: true, default: "No description provided" },
+    cost: { type: Number, min: 0, required: true },
+    category: { type: String, required: true },
     img: {
-        data: String,
-        contentType: String,
-        required: true
-    }
+        type: {
+            data: String,
+            contentType: String,
+        }, required: true
+    },
 })
 
 module.exports = mongoose.model("Product", productSchema);
